12f2a68620bebd518d947ec8048a7ff9be5c92a2
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2015-09-14 12:27:16 +0500

    switch to turf-shift-longitude

--- wrangel.js
+++ wrangel.js
@@ -1,49 +1,11 @@
-import clone from 'clone'
+import shift from 'turf-shift-longitude'
 import merge from 'turf-merge'
 import polygon from 'turf-polygon'
 import normalize from 'geojson-normalize'
 import collection from 'turf-featurecollection'
 
-var wrapPoint = function ([lng, lat]) {
-  if (lng < 0) lng = lng + 360
-  return [lng, lat]
-}
-
-var wrapLineString = function (coordinates) {
-  return coordinates.map(wrapPoint)
-}
-
-var wrapPolygon = function (coordinates) {
-  return coordinates.map(wrapLineString)
-}
-
-var wrapMultiPolygon = function (coordinates) {
-  return coordinates.map(wrapPolygon)
-}
-
-var wrappers = {
-  Point: wrapPoint,
-  // MultiPoint: wrapMultiPoint,
-  LineString: wrapLineString,
-  // MultiLineString: wrapMultiLineString,
-  Polygon: wrapPolygon,
-  MultiPolygon: wrapMultiPolygon,
-  // GeometryCollection: wrapGeometryCollection
-}
-
-var wrapFeature = function (feature) {
-  var wrappedFeature = clone(feature)
-  var wrapper = wrappers[wrappedFeature.geometry.type]
-
-  if (!wrapper) return feature
-
-  var wrappedCoordinates = wrapper(wrappedFeature.geometry.coordinates)
-  wrappedFeature.geometry.coordinates = wrappedCoordinates
-  return wrappedFeature
-}
-
 var wrap = function (feature) {
-  var wrappedFeature = wrapFeature(feature)
+  var wrappedFeature = shift(feature)
   var polygons = wrappedFeature.geometry.coordinates.map(polygon)
   return merge(collection(polygons))
 }
